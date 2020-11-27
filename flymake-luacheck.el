d09a1ee368bf4ba222d748c7788ca8ae6d8ba48e
David M. Cooke <david.m.cooke@gmail.com>
2020-11-27 13:52:29 -0800

    Updates (mostly descriptions)

--- flymake-luacheck.el
+++ flymake-luacheck.el
@@ -3,15 +3,40 @@
 ;; Copyright (C) 2020 David M. Cooke <david.m.cooke@gmail.com>
 
 ;; Author: David M. Cooke <david.m.cooke@gmail.com>
-;; Version: 0.1
+;; Version: 0.2
+;; Created: Oct. 14, 2020
 ;; Package-Requires: (flymake)
+;; SPDX-License-Identifier: MIT
 ;; Keywords: languages, lua
 
-;; <insert MIT license here>
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Commentary:
 
 ;; This package adds a flymake backend to check Lua files using luacheck.
+;; If luacheck is not available, luac will be used to check for syntax
+;; errors.
+;;
+;; Luacheck is a static analyzer and a linter for Lua. It is available
+;; from <https://github.com/mpeterv/luacheck>.
 ;;
 ;; The function `flymake-luacheck-setup' should be run in the buffer to
 ;; be checked before enabling `flymake-mode'.  For convenience, the
@@ -38,13 +63,6 @@
   :group 'flymake
   :prefix 'flymake-luacheck)
 
-(defcustom flymake-luacheck-dialect 'lua51
-  "The Lua dialect used when checking this buffer.
-XXX"
-  :group 'flymake-luacheck
-  :type '(symbol)
-  :local t)
-
 (defcustom flymake-luacheck-path
   (executable-find "luacheck")
   "Path to the 'luacheck' executable."
@@ -52,18 +70,42 @@ XXX"
   :type '(string)
   :risky t)
 
-(defcustom flymake-luacheck-standard-globals '(max)
-  "What luacheck considers to be the standard globals.
-XXX"
+(defcustom flymake-luacheck-standard-globals '(+ max)
+  "List of symbols setting what luacheck considers to be the standard globals.
+The symbols in the list are concatenated with a \"+\" separator,
+and passed to luacheck with the \"--std\" option.  The accepted
+symbols can be found in the luacheck documentation at
+URL `https://luacheck.readthedocs.io/en/stable/cli.html#command-line-options'.
+
+If the list starts with `+', then the sets of globals will be added to the
+current set (from a config file, for instance), rather than replacing it.
+
+The default `(+ max)' is equivalent to `(+ lua51c lua52c lua53c luajit)'.
+
+If the standard globals are set in a \".luacheckrc\" file, this should be
+set to `nil', which will inhibit adding a \"--std\" option to the luacheck
+command line. (Options on the command line overwrite those in config files.)
+"
   :group 'flymake-luacheck
   :type '(set symbol)
   :local t)
 
 (defcustom flymake-luacheck-extra-arguments nil
   "Extra arguments to pass to luacheck.
-The full set of arguments is constructed from `flymake-luacheck-standard-globals', this variable, and \"--formatter=plain --codes --ranges -\"."
+The full set of arguments is constructed from
+`flymake-luacheck-standard-globals', this variable, and
+\"--formatter=plain --codes --ranges -\"."
   :group 'flymake-luacheck
   :type '(set string)
+  :local t)
+
+(defcustom flymake-luacheck-dialect 'lua51
+  "The Lua dialect used when checking this buffer.
+
+This is only used by the fallback parser to determine which command for
+luac should be used."
+  :group 'flymake-luacheck
+  :type '(symbol)
   :local t)
 
 (defcustom flymake-luacheck-fallback-lua-parser-command
@@ -77,7 +119,7 @@ be either the symbol 'lua or 'luac, and the appropiate arguments will
 be appended to the end of the cdr.
 
 If this variable is nil, then a executable is determined from
-`flymake-luacheck-fallback-lua-parser-alist'."
+`flymake-luacheck-dialect' and `flymake-luacheck-fallback-lua-parser-alist'."
   :group 'flymake-luacheck
   :type '(choice (const :tag "No function" nil)
                  (function)))
@@ -107,7 +149,16 @@ argument is used.  The COMMAND should be the executable to run."
 This is added to the luacheck command line."
   (if flymake-luacheck-standard-globals
       (cons "--std"
-            (mapconcat #'symbol-name flymake-luacheck-standard-globals "+"))
+            (let* ((add-plus (eq (car flymake-luacheck-standard-globals) '+))
+                   (std (mapconcat
+                         #'symbol-name
+                         (if add-plus
+                             (cdr flymake-luacheck-standard-globals)
+                           flymake-luacheck-standard-globals)
+                         "+")))
+              (if add-plus
+                  (concat "+" std)
+                std)))
     nil))
 
 (defconst flymake-luacheck--loadfile-script
@@ -136,7 +187,7 @@ and the command is built from that."
                                              (cons prog (cdr cmd))))))
 
 (defun flymake-luacheck-command ()
-  "Build the luacheck command line, or nil if luacheck isn't found."
+  "Build the luacheck command line, or nil if luacheck or luac isn't found."
   (if-let ((prog (executable-find flymake-luacheck-path)))
       (list 'luacheck
             #'flymake-luacheck--match-errors
@@ -161,6 +212,8 @@ and the command is built from that."
               cmd)
       nil)))
 
+;;
+
 (defconst flymake-luacheck-err-re
   (rx bol (group (* (not ":")))         ; file name
       ":" (group (+ (any digit)))       ; line number
@@ -172,21 +225,41 @@ and the command is built from that."
           (+ space) (* nonl)) eol))                           ; message
   "Regular expression that matches luacheck's output.")
 
+(defun flymake-luacheck-line-column-region (source line start-col end-col)
+  "Get the region in SOURCE on line number LINE between START-COL and END-COL.
+The numbers are those returned by luacheck (1-based), and the columns
+include the end points.  The returned cons is half-open."
+  (with-current-buffer source
+    (save-excursion
+      (goto-char (point-min))
+      (forward-line (1- line))
+      (when (> start-col 0)
+        (move-to-column (1- start-col)))
+      (let ((start (point)))
+        (if (>= end-col start-col)
+            (progn
+              ;; end-col: 1- for 1-based, then 1+ for exclusive end point
+              (move-to-column end-col)
+              (cons start (point)))
+          (cons start start))))))
+
 (defun flymake-luacheck--match-errors (source)
   "Parse the error and warning messages from luacheck output.
 Argument SOURCE Source code buffer."
   (let (diags)
     (while (search-forward-regexp flymake-luacheck-err-re nil t)
       (let* ((lineno (string-to-number (match-string 2)))
-             (start-col (1- (string-to-number (match-string 3))))
-             (msg (match-string 4))
-             (type (if (null (match-string 5))
+             (start-col (string-to-number (match-string 3)))
+             (end-col (string-to-number (match-string 4)))
+             (msg (match-string 5))
+             (type (if (null (match-string 6))
                        :error ; more like fatal error
-                     (pcase (match-string 6)
+                     (pcase (match-string 7)
                        ("E" :error)
                        ("W" :warning)
                        (_ :error))))
-             (region (flymake-diag-region source lineno start-col)))
+             (region (flymake-luacheck-line-column-region
+                      source lineno start-col end-col)))
         (push (flymake-make-diagnostic source (car region)
                                        (cdr region) type msg)
               diags)))
@@ -218,7 +291,6 @@ Argument SOURCE Source code buffer."
 
 (defun flymake-luacheck--sentinel (match-errors source report-fn)
   "Return a process sentinel for the luacheck process.
-
 Argument MATCH-ERRORS function to match error messages in the output buffer.
 Argument SOURCE Source code buffer.
 Argument REPORT-FN Flymake callback."
@@ -228,8 +300,8 @@ Argument REPORT-FN Flymake callback."
             (if (with-current-buffer source (eq proc flymake-luacheck--proc))
                 (with-current-buffer (process-buffer proc)
                   (goto-char (point-min))
-                  (funcall report-fn
-                           (funcall match-errors source)))
+                  (let ((diags (funcall match-errors source)))
+                    (funcall report-fn diags)))
               (flymake-log :warning "Cancelling obsolete check %s" proc))
           (kill-buffer (process-buffer proc))))))
 
@@ -248,9 +320,9 @@ Argument REPORT-FN Flymake callback."
                (make-process
                 :name "luacheck-flymake" :noquery t
                 :connection-type 'pipe
-                :buffer (generate-new-buffer " *luacheck-flymake*")
+                :buffer (generate-new-buffer "*luacheck-flymake*")
                 :command cmdline
-                :sentinal (flymake-luacheck--sentinel
+                :sentinel (flymake-luacheck--sentinel
                            match-errors source report-fn)))
          (process-send-region flymake-luacheck--proc (point-min) (point-max))
          (process-send-eof flymake-luacheck--proc))))))
